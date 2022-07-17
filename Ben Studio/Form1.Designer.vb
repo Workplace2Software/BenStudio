cfad70f5a32e8a361986ff83789cd3f2faacae60
soafen <soafensoe@protonmail.com>
2022-07-17 19:11:00 +0200

    v1.0.0 Source
    Developed on the Trusty GovernmentPC in Brazil!

---
+++ Ben Studio/Form1.Designer.vb
@@ -0,0 +1,323 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Song6 = New System.Windows.Forms.Button()
+        Me.Song5 = New System.Windows.Forms.Button()
+        Me.Song4 = New System.Windows.Forms.Button()
+        Me.Song3 = New System.Windows.Forms.Button()
+        Me.Song2 = New System.Windows.Forms.Button()
+        Me.Song1 = New System.Windows.Forms.Button()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Go = New System.Windows.Forms.Button()
+        Me.Img6 = New System.Windows.Forms.Button()
+        Me.Img5 = New System.Windows.Forms.Button()
+        Me.Img4 = New System.Windows.Forms.Button()
+        Me.Img3 = New System.Windows.Forms.Button()
+        Me.Img2 = New System.Windows.Forms.Button()
+        Me.Img1 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Song6)
+        Me.GroupBox1.Controls.Add(Me.Song5)
+        Me.GroupBox1.Controls.Add(Me.Song4)
+        Me.GroupBox1.Controls.Add(Me.Song3)
+        Me.GroupBox1.Controls.Add(Me.Song2)
+        Me.GroupBox1.Controls.Add(Me.Song1)
+        Me.GroupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.GroupBox1.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.ForeColor = System.Drawing.Color.LimeGreen
+        Me.GroupBox1.Location = New System.Drawing.Point(387, 13)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(93, 240)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Song"
+        '
+        'Song6
+        '
+        Me.Song6.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.Song6.FlatAppearance.BorderSize = 3
+        Me.Song6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Song6.Font = New System.Drawing.Font("Comic Sans MS", 9.0!, System.Drawing.FontStyle.Bold)
+        Me.Song6.ForeColor = System.Drawing.Color.LimeGreen
+        Me.Song6.Location = New System.Drawing.Point(6, 199)
+        Me.Song6.Name = "Song6"
+        Me.Song6.Size = New System.Drawing.Size(81, 28)
+        Me.Song6.TabIndex = 7
+        Me.Song6.Text = "Six"
+        Me.Song6.UseVisualStyleBackColor = True
+        '
+        'Song5
+        '
+        Me.Song5.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.Song5.FlatAppearance.BorderSize = 3
+        Me.Song5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Song5.Font = New System.Drawing.Font("Comic Sans MS", 9.0!, System.Drawing.FontStyle.Bold)
+        Me.Song5.ForeColor = System.Drawing.Color.LimeGreen
+        Me.Song5.Location = New System.Drawing.Point(6, 165)
+        Me.Song5.Name = "Song5"
+        Me.Song5.Size = New System.Drawing.Size(81, 28)
+        Me.Song5.TabIndex = 7
+        Me.Song5.Text = "Five"
+        Me.Song5.UseVisualStyleBackColor = True
+        '
+        'Song4
+        '
+        Me.Song4.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.Song4.FlatAppearance.BorderSize = 3
+        Me.Song4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Song4.Font = New System.Drawing.Font("Comic Sans MS", 9.0!, System.Drawing.FontStyle.Bold)
+        Me.Song4.ForeColor = System.Drawing.Color.LimeGreen
+        Me.Song4.Location = New System.Drawing.Point(6, 131)
+        Me.Song4.Name = "Song4"
+        Me.Song4.Size = New System.Drawing.Size(81, 28)
+        Me.Song4.TabIndex = 7
+        Me.Song4.Text = "Four"
+        Me.Song4.UseVisualStyleBackColor = True
+        '
+        'Song3
+        '
+        Me.Song3.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.Song3.FlatAppearance.BorderSize = 3
+        Me.Song3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Song3.Font = New System.Drawing.Font("Comic Sans MS", 9.0!, System.Drawing.FontStyle.Bold)
+        Me.Song3.ForeColor = System.Drawing.Color.LimeGreen
+        Me.Song3.Location = New System.Drawing.Point(6, 97)
+        Me.Song3.Name = "Song3"
+        Me.Song3.Size = New System.Drawing.Size(81, 28)
+        Me.Song3.TabIndex = 7
+        Me.Song3.Text = "Three"
+        Me.Song3.UseVisualStyleBackColor = True
+        '
+        'Song2
+        '
+        Me.Song2.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.Song2.FlatAppearance.BorderSize = 3
+        Me.Song2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Song2.Font = New System.Drawing.Font("Comic Sans MS", 9.0!, System.Drawing.FontStyle.Bold)
+        Me.Song2.ForeColor = System.Drawing.Color.LimeGreen
+        Me.Song2.Location = New System.Drawing.Point(6, 63)
+        Me.Song2.Name = "Song2"
+        Me.Song2.Size = New System.Drawing.Size(81, 28)
+        Me.Song2.TabIndex = 7
+        Me.Song2.Text = "Two"
+        Me.Song2.UseVisualStyleBackColor = True
+        '
+        'Song1
+        '
+        Me.Song1.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.Song1.FlatAppearance.BorderSize = 3
+        Me.Song1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Song1.Font = New System.Drawing.Font("Comic Sans MS", 9.0!, System.Drawing.FontStyle.Bold)
+        Me.Song1.ForeColor = System.Drawing.Color.LimeGreen
+        Me.Song1.Location = New System.Drawing.Point(6, 29)
+        Me.Song1.Name = "Song1"
+        Me.Song1.Size = New System.Drawing.Size(81, 28)
+        Me.Song1.TabIndex = 7
+        Me.Song1.Text = "One"
+        Me.Song1.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Img6)
+        Me.GroupBox2.Controls.Add(Me.Img5)
+        Me.GroupBox2.Controls.Add(Me.Img4)
+        Me.GroupBox2.Controls.Add(Me.Img3)
+        Me.GroupBox2.Controls.Add(Me.Img2)
+        Me.GroupBox2.Controls.Add(Me.Img1)
+        Me.GroupBox2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.GroupBox2.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.ForeColor = System.Drawing.Color.LimeGreen
+        Me.GroupBox2.Location = New System.Drawing.Point(13, 255)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(368, 114)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Image"
+        '
+        'Go
+        '
+        Me.Go.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.Go.FlatAppearance.BorderSize = 3
+        Me.Go.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Go.Font = New System.Drawing.Font("Comic Sans MS", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Go.ForeColor = System.Drawing.Color.LimeGreen
+        Me.Go.Location = New System.Drawing.Point(387, 267)
+        Me.Go.Name = "Go"
+        Me.Go.Size = New System.Drawing.Size(93, 102)
+        Me.Go.TabIndex = 2
+        Me.Go.Text = "Go"
+        Me.Go.UseVisualStyleBackColor = True
+        '
+        'Img6
+        '
+        Me.Img6.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.Img6.FlatAppearance.BorderSize = 3
+        Me.Img6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Img6.Font = New System.Drawing.Font("Comic Sans MS", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Img6.ForeColor = System.Drawing.Color.LimeGreen
+        Me.Img6.Image = Global.Ben_Studio.My.Resources.Resources.BTN6
+        Me.Img6.Location = New System.Drawing.Point(305, 26)
+        Me.Img6.Name = "Img6"
+        Me.Img6.Size = New System.Drawing.Size(56, 74)
+        Me.Img6.TabIndex = 7
+        Me.Img6.UseVisualStyleBackColor = True
+        '
+        'Img5
+        '
+        Me.Img5.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.Img5.FlatAppearance.BorderSize = 3
+        Me.Img5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Img5.Font = New System.Drawing.Font("Comic Sans MS", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Img5.ForeColor = System.Drawing.Color.LimeGreen
+        Me.Img5.Image = Global.Ben_Studio.My.Resources.Resources.BTN5
+        Me.Img5.Location = New System.Drawing.Point(245, 26)
+        Me.Img5.Name = "Img5"
+        Me.Img5.Size = New System.Drawing.Size(56, 74)
+        Me.Img5.TabIndex = 6
+        Me.Img5.UseVisualStyleBackColor = True
+        '
+        'Img4
+        '
+        Me.Img4.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.Img4.FlatAppearance.BorderSize = 3
+        Me.Img4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Img4.Font = New System.Drawing.Font("Comic Sans MS", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Img4.ForeColor = System.Drawing.Color.LimeGreen
+        Me.Img4.Image = Global.Ben_Studio.My.Resources.Resources.BTN4
+        Me.Img4.Location = New System.Drawing.Point(185, 26)
+        Me.Img4.Name = "Img4"
+        Me.Img4.Size = New System.Drawing.Size(56, 74)
+        Me.Img4.TabIndex = 5
+        Me.Img4.UseVisualStyleBackColor = True
+        '
+        'Img3
+        '
+        Me.Img3.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.Img3.FlatAppearance.BorderSize = 3
+        Me.Img3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Img3.Font = New System.Drawing.Font("Comic Sans MS", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Img3.ForeColor = System.Drawing.Color.LimeGreen
+        Me.Img3.Image = Global.Ben_Studio.My.Resources.Resources.BTN3
+        Me.Img3.Location = New System.Drawing.Point(125, 26)
+        Me.Img3.Name = "Img3"
+        Me.Img3.Size = New System.Drawing.Size(56, 74)
+        Me.Img3.TabIndex = 4
+        Me.Img3.UseVisualStyleBackColor = True
+        '
+        'Img2
+        '
+        Me.Img2.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.Img2.FlatAppearance.BorderSize = 3
+        Me.Img2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Img2.Font = New System.Drawing.Font("Comic Sans MS", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Img2.ForeColor = System.Drawing.Color.LimeGreen
+        Me.Img2.Image = Global.Ben_Studio.My.Resources.Resources.BTN2
+        Me.Img2.Location = New System.Drawing.Point(65, 26)
+        Me.Img2.Name = "Img2"
+        Me.Img2.Size = New System.Drawing.Size(56, 74)
+        Me.Img2.TabIndex = 3
+        Me.Img2.UseVisualStyleBackColor = True
+        '
+        'Img1
+        '
+        Me.Img1.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.Img1.FlatAppearance.BorderSize = 3
+        Me.Img1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Img1.Font = New System.Drawing.Font("Comic Sans MS", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Img1.ForeColor = System.Drawing.Color.LimeGreen
+        Me.Img1.Image = Global.Ben_Studio.My.Resources.Resources.BTN1
+        Me.Img1.Location = New System.Drawing.Point(5, 26)
+        Me.Img1.Name = "Img1"
+        Me.Img1.Size = New System.Drawing.Size(56, 74)
+        Me.Img1.TabIndex = 2
+        Me.Img1.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Black
+        Me.PictureBox1.Location = New System.Drawing.Point(13, 13)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(368, 240)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1605
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DarkGreen
+        Me.ClientSize = New System.Drawing.Size(492, 385)
+        Me.Controls.Add(Me.Go)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(498, 413)
+        Me.MinimumSize = New System.Drawing.Size(498, 413)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Ben Studio **UNREGISTERED**"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents Go As System.Windows.Forms.Button
+    Friend WithEvents Img6 As System.Windows.Forms.Button
+    Friend WithEvents Img5 As System.Windows.Forms.Button
+    Friend WithEvents Img4 As System.Windows.Forms.Button
+    Friend WithEvents Img3 As System.Windows.Forms.Button
+    Friend WithEvents Img2 As System.Windows.Forms.Button
+    Friend WithEvents Img1 As System.Windows.Forms.Button
+    Friend WithEvents Song6 As System.Windows.Forms.Button
+    Friend WithEvents Song5 As System.Windows.Forms.Button
+    Friend WithEvents Song4 As System.Windows.Forms.Button
+    Friend WithEvents Song3 As System.Windows.Forms.Button
+    Friend WithEvents Song2 As System.Windows.Forms.Button
+    Friend WithEvents Song1 As System.Windows.Forms.Button
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ Ben Studio/Form1.vb
@@ -0,0 +1,150 @@
+﻿Public Class Form1
+    Dim r As New Random
+    Dim img As Integer
+    Dim tune As Integer
+    Private Sub Song1_MouseHover(ByVal sender As Object, ByVal e As System.EventArgs) Handles Song1.MouseHover
+        My.Computer.Audio.Play(My.Resources.Tune1, AudioPlayMode.Background)
+    End Sub
+
+    Private Sub Song2_MouseHover(ByVal sender As Object, ByVal e As System.EventArgs) Handles Song2.MouseHover
+        My.Computer.Audio.Play(My.Resources.Tune2, AudioPlayMode.Background)
+    End Sub
+
+    Private Sub Song3_MouseHover(ByVal sender As Object, ByVal e As System.EventArgs) Handles Song3.MouseHover
+        My.Computer.Audio.Play(My.Resources.Tune3, AudioPlayMode.Background)
+    End Sub
+
+    Private Sub Song4_MouseHover(ByVal sender As Object, ByVal e As System.EventArgs) Handles Song4.MouseHover
+        My.Computer.Audio.Play(My.Resources.Tune4, AudioPlayMode.Background)
+    End Sub
+
+    Private Sub Song5_MouseHover(ByVal sender As Object, ByVal e As System.EventArgs) Handles Song5.MouseHover
+        My.Computer.Audio.Play(My.Resources.Tune5, AudioPlayMode.Background)
+    End Sub
+
+    Private Sub Song6_MouseHover(ByVal sender As Object, ByVal e As System.EventArgs) Handles Song6.MouseHover
+        My.Computer.Audio.Play(My.Resources.Tune6, AudioPlayMode.Background)
+    End Sub
+
+    Private Sub Go_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Go.Click
+        My.Computer.Audio.Play(My.Resources.Selection, AudioPlayMode.WaitToComplete)
+        Select Case img
+            Case 1
+                PictureBox1.Image = My.Resources.Ben1
+            Case 2
+                PictureBox1.Image = My.Resources.Ben2
+            Case 3
+                PictureBox1.Image = My.Resources.Ben3
+            Case 4
+                PictureBox1.Image = My.Resources.Ben4
+            Case 5
+                PictureBox1.Image = My.Resources.Ben5
+            Case 6
+                PictureBox1.Image = My.Resources.Ben6
+        End Select
+        Application.DoEvents()
+        Select Case tune
+            Case 1
+                My.Computer.Audio.Play(My.Resources.Tune1, AudioPlayMode.WaitToComplete)
+            Case 2
+                My.Computer.Audio.Play(My.Resources.Tune2, AudioPlayMode.WaitToComplete)
+            Case 3
+                My.Computer.Audio.Play(My.Resources.Tune3, AudioPlayMode.WaitToComplete)
+            Case 4
+                My.Computer.Audio.Play(My.Resources.Tune4, AudioPlayMode.WaitToComplete)
+            Case 5
+                My.Computer.Audio.Play(My.Resources.Tune5, AudioPlayMode.WaitToComplete)
+            Case 6
+                My.Computer.Audio.Play(My.Resources.Tune6, AudioPlayMode.WaitToComplete)
+        End Select
+        If img = 1 And tune = 1 And My.Settings.Pro = False Then
+            My.Computer.Audio.PlaySystemSound(Media.SystemSounds.Asterisk)
+            MessageBox.Show("Happy Ben is not available in the Free version of Ben Studio. Please go to our website to purchase the full version.")
+            Me.Hide()
+            My.Computer.Audio.Play(My.Resources.ExpressingAnger, AudioPlayMode.Background)
+            Timer1.Start()
+        End If
+        PictureBox1.Image = Nothing
+    End Sub
+
+    Private Sub Song1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Song1.Click
+        My.Computer.Audio.Play(My.Resources.Selection, AudioPlayMode.Background)
+        tune = 1
+    End Sub
+
+    Private Sub Song2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Song2.Click
+        My.Computer.Audio.Play(My.Resources.Selection, AudioPlayMode.Background)
+        tune = 2
+    End Sub
+
+    Private Sub Song3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Song3.Click
+        My.Computer.Audio.Play(My.Resources.Selection, AudioPlayMode.Background)
+        tune = 3
+    End Sub
+
+    Private Sub Song4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Song4.Click
+        My.Computer.Audio.Play(My.Resources.Selection, AudioPlayMode.Background)
+        tune = 4
+    End Sub
+
+    Private Sub Song5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Song5.Click
+        My.Computer.Audio.Play(My.Resources.Selection, AudioPlayMode.Background)
+        tune = 5
+    End Sub
+
+    Private Sub Song6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Song6.Click
+        My.Computer.Audio.Play(My.Resources.Selection, AudioPlayMode.Background)
+        tune = 6
+    End Sub
+
+    Private Sub Img1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Img1.Click
+        My.Computer.Audio.Play(My.Resources.Selection, AudioPlayMode.Background)
+        img = 1
+    End Sub
+
+    Private Sub Img2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Img2.Click
+        My.Computer.Audio.Play(My.Resources.Selection, AudioPlayMode.Background)
+        img = 2
+    End Sub
+
+    Private Sub Img3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Img3.Click
+        My.Computer.Audio.Play(My.Resources.Selection, AudioPlayMode.Background)
+        img = 3
+    End Sub
+
+    Private Sub Img4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Img4.Click
+        My.Computer.Audio.Play(My.Resources.Selection, AudioPlayMode.Background)
+        img = 4
+    End Sub
+
+    Private Sub Img5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Img5.Click
+        My.Computer.Audio.Play(My.Resources.Selection, AudioPlayMode.Background)
+        img = 5
+    End Sub
+
+    Private Sub Img6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Img6.Click
+        My.Computer.Audio.Play(My.Resources.Selection, AudioPlayMode.Background)
+        img = 6
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Application.Exit()
+        End
+    End Sub
+
+    Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        If r.Next(1, 3) = 1 Then
+            Try
+                Process.Start("C:\Program Files\Workplace2Software\BenStudio\GoPro.jpg")
+            Catch ex As Exception
+                MessageBox.Show("By going pro, you can:" & Environment.NewLine & Environment.NewLine & "* Unlock all Bens (including canny ben)" & Environment.NewLine & "* Unlock the ability to install Super Idol Canny pack. (uncanny not included)" & Environment.NewLine & "* Unlock technical support" & Environment.NewLine & Environment.NewLine & "All of this, AND MORE from only **7 easy payments** of $79.99!" & Environment.NewLine & "Order TODAY at theworkplace.tk/ben-studio")
+            End Try
+        End If
+    End Sub
+
+    Private Sub Form1_Shown(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Shown
+        If My.Settings.Pro = True Then
+            Me.Text = "Ben Studio (Registered)"
+        End If
+    End Sub
+End Class
